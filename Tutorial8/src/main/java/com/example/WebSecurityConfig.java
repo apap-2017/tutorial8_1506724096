3c504c33aa26755451c11412729a050967096768
Fajar Marseto <fajar.marseto28@gmail.com>
2017-11-18 21:53:48 +0700

    ​Final

--- Tutorial8/src/main/java/com/example/WebSecurityConfig.java
+++ Tutorial8/src/main/java/com/example/WebSecurityConfig.java
@@ -14,9 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/student/**").hasRole("USER")
-				.antMatchers("/course/**").hasRole("ADMIN").anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/course/viewall").hasRole("ADMIN")
+				.antMatchers("/student/viewall").hasAnyRole("USER", "ADMIN").anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 	// @Autowired
